7de93f2a715c8db949faab5ca3b9418ad98561d0
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-07-24 21:44:11 +0200

    Create MyInt class with inverted comparison operators

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""MyInt class with inverted == and != operators"""
+
+
+class MyInt(int):
+    """MyInt class with inverted == and != operators"""
+
+    def __eq__(self, other):
+        """Invert == operator"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Invert != operator"""
+        return super().__eq__(other)
