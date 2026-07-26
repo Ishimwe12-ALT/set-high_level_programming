4e12ef37fda4c52e05df6ec03713d24374ff11ec
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-07-26 14:00:16 -0700

    Fix PEP8 line length and line count

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-str = ("Python is an interpreted, interactive, object-oriented " +
-       "programming language that combines remarkable power with " +
-       "very clear syntax")
-str = str[39:67] + str[107:112] + str[:6]
+word = "object-oriented programming with Python"
+print(word[0:26] + word[31:36] + word[-6:])
 print(str)
