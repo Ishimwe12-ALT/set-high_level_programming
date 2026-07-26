d5c1215282b375150cf570563bdb2900e379e4de
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-07-27 00:39:11 +0200

    Refactor inherits_from to use issubclass

--- python-inheritance/ 4-inherits_from.py
+++ python-inheritance/ 4-inherits_from.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Function to check if object is an instance of a class that inherited from"""
+"""
+Defines a function that checks if an object is an instance of a class
+that inherited (directly or indirectly) from a specified class.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited from
-    a_class
-    """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    """Returns True if obj is an instance of a subclass of a_class."""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
