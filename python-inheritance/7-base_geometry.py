ca3b470d24e237a4d51f6904f9910dae1e734fc4
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-07-27 00:42:15 +0200

    Refine docstrings in BaseGeometry class
    Updated docstrings for clarity and added argument details.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""BaseGeometry class with integer validation"""
+"""Defines a base geometry class."""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """Represent a base geometry."""
 
     def area(self):
-        """Raise an exception when called"""
+        """Raise an Exception with a message."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate that value is an integer greater than 0"""
+        """Validate that value is a valid integer.
+
+        Args:
+            name (str): The name of the attribute.
+            value (int): The value to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
