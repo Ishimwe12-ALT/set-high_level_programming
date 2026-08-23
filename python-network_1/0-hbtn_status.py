1d54d140c745b2c77853c00f4f573faf38877593
Ishimwe12-ALT <dukundanesandra51@gmail.com>
2026-08-23 07:41:47 -0700

    Fix 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import urllib.request
+
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        content = response.read()
+        html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
